730ba3d9f4935851749ee426e45cb822e8e39e07
George Matter <george.matter.vi@gmail.com>
2015-06-29 20:46:02 -0700

    add uploads bits to push/pull scripts

--- pull.sh
+++ pull.sh
@@ -1,2 +1,3 @@
 ssh theveraproject.org mysqldump -p'@aDD6m3Ng@' --add-drop-table wordpress | mysql wordpress
 echo 'update wp_options set option_value="http://hedberg" where option_name in ("siteurl", "home");' | mysql wordpress
+sudo scp -r theveraproject.org:/var/www/wordpress/wp-content/uploads /var/www/wordpress/wp-content/uploads
--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 mysqldump --add-drop-table wordpress | ssh theveraproject.org mysql -p'@aDD6m3Ng@' wordpress
 echo 'update wp_options set option_value="http://new.theveraproject.org" where option_name in ("siteurl", "home");' | ssh theveraproject.org mysql -p'@aDD6m3Ng@' wordpress
+sudo scp -r /var/www/wordpress/wp-content/uploads theveraproject.org:/var/www/wordpress/wp-content/uploads
