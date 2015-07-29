3a297b745c4af2673c4197169fec8197b160c2a0
George Matter <george.matter.vi@gmail.com>
2015-07-29 13:36:42 -0700

    fix push.sh rsync command

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 mysqldump --add-drop-table wordpress | ssh theveraproject.org mysql -p'@aDD6m3Ng@' wordpress
 echo 'update wp_options set option_value="http://new.theveraproject.org" where option_name in ("siteurl", "home");' | ssh theveraproject.org mysql -p'@aDD6m3Ng@' wordpress
-rsync -rz /var/www/wordpress/wp-content/uploads theveraproject.org:/var/www/wordpress/wp-content/uploads
+rsync -rz /var/www/wordpress/wp-content/uploads/ theveraproject.org:/var/www/wordpress/wp-content/uploads/
 ssh theveraproject.org "cd ~/vera-wordpress/ && git pull"
