1b482e30c89f5b3fef152b31709c94feaa93812c
George Matter <george.matter.vi@gmail.com>
2015-10-22 00:26:59 -0400

    restructure plugins

---
+++ plugins/index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: The Vera Project
+*/
+
+include( plugin_dir_path( __FILE__ ) . 'classes/index.php');
+include( plugin_dir_path( __FILE__ ) . 'no-taxonomies/index.php');
+include( plugin_dir_path( __FILE__ ) . 'gallery/index.php');
