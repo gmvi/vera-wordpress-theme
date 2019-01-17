91bcee99d95dd654fd5dec5de0e821dfd81bef9e
Jessi Shank <jessishank1@gmail.com>
2019-01-17 12:14:57 -0800

    finished hero, submenu, blurb, current show for gallery

---
+++ page-gallery.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Get Involved template.
+ *
+ */
+
+get_header();
+
+// Get container type from Wordpress Customizer
+$container = get_theme_mod( 'understrap_container_type' );
+
+
+$gallery_img="";
+if (has_post_thumbnail($post ->ID)):
+    $gallery_img = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail')[0];
+endif;
+
+
+function pad_zeroes( $num ) {
+    if ( $num > 9 ) {
+        return $num;
+    }
+
+    return str_pad( $num, 2, '0', STR_PAD_LEFT );
+}
+
+?>
+<div class="wrapper" id="index-wrapper">
+
+    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+        <main class="site-main" id="main">
+            <?php the_post(); // This is a page template ?>
+            <?php get_template_part('partial-templates/titlecard-fullwidth'); ?>
+            <section class="entry-header">
+                <div class="menu">
+                    <!-- TODO: once we know more about this sub menu from george swap this out for something more reusable -->
+                    <!-- TODO QUESTION: this looks weird af on mobile but i don't know what it should look like, should the blue bar expand vertically on mobile?-->
+                    <div class="menu-item current-menu-item"><a>overview</a></div>
+                    <div class="menu-item"><a href="/" class="menu-item">classes</a></div>
+                    <div class="menu-item"><a href="/" class="menu-item">recording studio</a></div>
+                </div>
+            </section>
+            <?php get_template_part('partial-templates/pageblurb'); ?>
+            <section>
+                <div class="row  no-gutters current-gallery">
+                    <div class="col-sm-6">
+                        <div class="row align-items-center h-100">
+                            <div class="col-9 mx-auto">
+                                <p class="label">Current Show</p>
+                                <h2 class="large-header"><?php the_field( 'current_gallery_header' ); ?></h2>
+                                <p><?php the_field('current_gallery_description'); ?></p>
+                                <p><b>Exhibit up until <?php the_field('current_gallery_end_date'); ?><br>
+                                        Opening: <?php the_field('current_gallery_opening_datetime'); ?></b></p>
+                                <a href="<?php the_field('current_gallery_link'); ?>" class="more">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <img class="current-gallery-image" src="<?php echo get_field( 'current_gallery_image' )['url'];?>" />
+                    </div>
+                </div>
+            </section>
+
+        </main><!-- #main -->
+
+    </div><!-- #content -->
+
+</div><!-- .wrapper -->
+
+
+<?php get_footer(); ?>
---
+++ partial-templates/pageblurb.php
@@ -0,0 +1,14 @@
+<div class="entry-content text-center">
+    <section class="info">
+        <div class="row header">
+            <div class="col-md-12">
+                <h3><?php the_field('header'); ?></h3>
+            </div>
+        </div><!-- .section-header -->
+        <div class="row body">
+            <div class="col-md-12">
+                <?php the_content(); ?>
+            </div>
+        </div><!-- .section-body -->
+    </section><!-- .section-main -->
+</div>
