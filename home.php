b5f41f037b6b92fda479515ec5860da2d4881062
Jessi Shank <jessishank1@gmail.com>
2019-01-22 17:20:12 -0800

    starting blog overview page

--- home.php
+++ home.php
@@ -1,33 +1,48 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package understrap
- */
+<?php get_header(); ?>
+    <div class="container-fluid">
+        <section class="row justify-content-md-center">
+            <div class="col-md-8 text-center">
+                <p class="label">News and Stories</p>
+                <h2 class="medium-header">Happening at Vera</h2>
+            </div>
+        </section>
+        <section class="row justify-content-md-center">
+            <div class="col-md-10 text-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+            </div>
+        </section>
+        <?php
+        $numOfCols = 3;
+        $rowCount = 0;
+        $bootstrapColWidth = 12 / $numOfCols;
+        $count = 0;
+        ?>
+        <div class="row">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); $count++; ?>
+            <div class="col-md-<?php echo $bootstrapColWidth; ?>">
+                <div class="card-deck">
+                    <div class="card">
+                        <img class="card-img-top" style="object-fit:cover;" src="<?php echo the_post_thumbnail_url('medium')?>" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo get_the_date(); ?></h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php
+        if($count % $numOfCols == 0) echo '</div><div class="row">';
+        ?>
+        <?php endwhile; ?>
+        <?php else : ?>
+            <div class="col-md-12 mx-auto">
+                <h1>No posts were found.</h1>
+            </div>
+        </div>
+        <?php endif; ?>
+        </div>
+    </div>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-get_header();
-
-$container   = get_theme_mod( 'understrap_container_type' );
-?>
-
-<div class="wrapper" id="index-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
-
-
-	</div><!-- Container end -->
-
-</div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
