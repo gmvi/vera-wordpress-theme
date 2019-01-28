e00a6db90ed58e73a7388fc1dc3601fc62023330
George Matter <George.Matter.VI@gmail.com>
2019-01-27 23:10:36 -0800

    Sort shows by date

--- plugins/shows/index.php
+++ plugins/shows/index.php
@@ -30,22 +30,49 @@ function vera_shows_init() {
       'publicly_queryable' => false,
       'show_in_nav_menus' => false,
       'rewrite' => false,
-      'supports' => array( 'title', 'editor', 'thumbnail', 'exerpt' ),
+      'supports' => array( 'title', 'editor', 'thumbnail', 'exerpt', 'page-attributes' ),
     )
   );
 }
 
-function vera_shows_page_configuration() {
+add_action( 'pre_get_posts', 'vera_shows_order_by_date' );
+function vera_shows_order_by_date($query) {
+  if ($query->get('post_type') == 'shows') {
+    if ($query->get('order') == '' && $query->get('meta_key') == '') {
+      $query->set('order', 'asc');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('meta_key', 'date');
+    }
+  }
+}
+
+add_filter( 'manage_shows_posts_columns', 'vera_shows_columns' );
+function vera_shows_columns($columns) {
+  $columns['show_date'] = "Show Date";
+  unset($columns['date']);
+  $columns['date'] = "Edit Date";
+  return $columns;
+}
+
+add_action( 'manage_shows_posts_custom_column' , 'vera_shows_column', 10, 2 );
+function vera_shows_column( $column, $post_id ) {
+  switch ($column) {
+    case 'show_date':
+      echo get_field('date', $post_id);
+      break;
+  }
+}
+
+function vera_shows_page_import() {
   echo '<div class="wrap">';
   echo '<h2>Shows Page Configuration</h2>';
   echo '<h3>Under Construction</h3>';
   echo '</div>';
 }
-
 function vera_shows_admin_init() {
-  add_submenu_page( 'edit.php?post_type=show', 'Shows Page Configuration', 'Shows Page',
-    'read', 'shows_page', 'vera_shows_page_configuration' );
+  add_submenu_page( 'edit.php?post_type=shows', 'Import Shows', 'Import Shows',
+    'edit_posts', 'import_shows', 'vera_shows_page_import' );
 }
-//add_action( 'admin_menu', 'vera_classes_admin_init');
+add_action( 'admin_menu', 'vera_shows_admin_init');
 
 ?>
