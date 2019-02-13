57cdbea3d5a49ac77ef2dd4104b9b21a1f3352de
Marianne Feng <feng.marianne@gmail.com>
2019-02-12 19:26:26 -0800

    trying to add sticky submenu now

--- src/js/sticky-header.js
+++ src/js/sticky-header.js
@@ -6,10 +6,13 @@
 ( function( $ ) {
 
     var offset = $( "#wrapper-navbar" ).offset();
+    var submenuOffset = $( "#centered-submenu" ).offset();
+
     checkOffset();
 
     $(window).scroll(function() {
         checkOffset();
+        console.log('scrolly at', $(document).scrollTop(), 'top offset', offset, 'sub offset', submenuOffset);
     });
 
     function checkOffset() {
@@ -19,6 +22,14 @@
         } else {
             $('#wrapper-navbar').removeClass('fixed-top');
             $('.nav-spacer').hide(0);
+        }
+
+        if ( $(document).scrollTop() - offset.top > submenuOffset.top){
+            $('#centered-submenu').addClass('fixed-top');
+            $('.submenu-spacer').show(0);
+        } else {
+            $('#centered-submenu').removeClass('fixed-top');
+            $('.submenu-spacer').hide(0);
         }
     }
 
