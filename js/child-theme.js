7e2c571280413396a1b3c2257adce4ca46b9cc02
Marianne Feng <feng.marianne@gmail.com>
2019-06-05 13:18:03 -0700

    in the middle of doing search behavior still

--- js/child-theme.js
+++ js/child-theme.js
@@ -4080,3 +4080,31 @@
     }
 
 } )( jQuery );
+
+/**
+ * Handles search behavior in nav (initially collapsed, then expand on click)
+ *
+* */
+
+( function( $ ) {
+
+    var openSearch = false;
+
+
+    $( "form#searchform" ).on('submit', function(e) {
+        var inputVal = $( "input#vera-search" ).val();
+        
+
+
+        if (!openSearch) {
+            e.preventDefault();
+            $( "input#vera-search" ).removeClass("d-none");
+            $( "button#searchsubmit" ).removeClass("border-0");
+            openSearch = true;
+        } else {
+            console.log('about to submit form');
+            $( "form#searchform" ).submit();
+        }
+    });
+
+} )( jQuery );
--- src/js/search.js
+++ src/js/search.js
@@ -9,9 +9,15 @@
 
 
     $( "form#searchform" ).on('submit', function(e) {
+        var inputVal = $( "input#vera-search" ).val();
+        
+
+
         if (!openSearch) {
             e.preventDefault();
             $( "input#vera-search" ).removeClass("d-none");
+            $( "button#searchsubmit" ).removeClass("border-0");
+            openSearch = true;
         } else {
             console.log('about to submit form');
             $( "form#searchform" ).submit();
