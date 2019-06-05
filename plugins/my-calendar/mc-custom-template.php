0be817e5b8212bf427a5135ab7821db209915fe7
Jessi Shank <jessishank1@gmail.com>
2019-06-05 13:08:39 -0700

    tweaks for list view

--- plugins/my-calendar/mc-custom-template.php
+++ plugins/my-calendar/mc-custom-template.php
@@ -6,6 +6,7 @@
  * Time: 19:36
  */
 
+
 /**
  * This plug-in demonstrates adding a custom template in PHP.
  *
@@ -26,50 +27,50 @@ add_filter( 'mc_custom_template', 'my_custom_calendar', 10, 7);
 function my_custom_calendar( $body = false, $data, $event, $type, $process_date, $time, $template ) {
 //    error_log("event is " . json_encode($event), 0 );
     error_log('type is ' . json_encode($type), 0);
-    if ( 'single' == $type || 'list' == $type) {
-//        error_log(json_encode($event),0);
-//        error_log('dataaaa \n\n' . json_encode($data), 0);
-        // only do the pretty header block with image if it is a single type, if its a list then it will generate the block
-        // for _every_ item, so you get... n header blocks
-        $description = $data['description'];
-        $address = $data['hcard'];
-        $date = $data['daterange'];
-        $timeslot = $data['timerange'];
-        $featuredPic = $data['medium_large_url'];
-        $title = $data['title'];
-        $featuredPicRow = '';
-        $headerTitle = '';
-        $dividingRules = '';
-        // if the event is a list instead of a single, then image & title won't be showing in the block header set by `single-mc-events.php`,
-        //    so go ahead and add it above the content
-        if ( 'list' == $type) {
-            $dividingRules = '<hr/>';
-            $featuredPicRow = $featuredPic ? "<div class='row mx-4'><img src='$featuredPic' class='img-fluid mx-auto' style='object-fit: contain;max-height:300px;' alt='$title'></div>" : '';
-            $headerTitle = "<h3>$title</h3>";
-        }
-        $body = <<<EOT
-$dividingRules
-$featuredPicRow
+    switch ($type) {
+        case 'single':
+            $real_begin_date = ( isset( $event->occur_begin ) ) ? $event->occur_begin : $event->event_begin . ' ' . $event->event_time;
+            $description = $data['description'];
+            $address = $data['hcard'];
+            $date = $data['daterange'];
+            $timeslot = $data['timerange'];
+            $recurs = mc_event_recur_string($event, $real_begin_date);
+            // don't show reccurence if there is no reccurence
+            $recurs_div = $recurs != "Does not recur" ? "<div>$recurs</div>" : '';
+//            error_log("recurs? " . json_encode($event->event_repeats));
+            $body = <<<EOT
 <div class="row mx-4">
-    <div class="col-md-9">$headerTitle $description</div>
+    <div class="col-md-9">$description</div>
     <div class="col-md-3 text-right">
         <div>$date</div>
         <div>$timeslot</div>
         <hr class="ml-4"/>
         <div>$address</div>
+        $recurs_div
     </div>
 </div>
-$dividingRules
 EOT;
-//        return $body;
-    } else if ( 'mini' == $type ) {
-        // if it's the jquery popup from the mini calendar, just show minimal event details and links
-        $body = '<div class="mini-event-detail py-1 py-sm-3">
-    <div class="row mx-2 mx-md-4"> 
-        <div class="col-7 col-md-9"><h4>' . $data['title'] . '</h4></div>
-        <div class="col-5 col-md-3"><a href="'. $data['details_link'] .'"><b>'. $data['timerange'] . '</b></a></div>
+            break;
+        case 'list':
+            $num_words = 65;
+            $excerpt   = wp_trim_words( $data['description'], $num_words );
+            $body = '<div class="row">
+    <div class="card w-100 mx-3 mb-2">
+      <div class="card-body">
+        <h3 class="card-title">' . $data['title'] . '</h3>
+        <p class="card-text">' . $excerpt . '</p>
+        <a href="#" class="btn btn-primary">Button</a>
+      </div>
     </div>
 </div>';
+            break;
+        case 'mini':
+            $body = '<div class="mini-event-detail py-1 py-sm-3">
+                        <div class="row mx-2 mx-md-4"> 
+                            <div class="col-7 col-md-9"><h4>' . $data['title'] . '</h4></div>
+                            <div class="col-5 col-md-3"><a href="'. $data['details_link'] .'"><b>'. $data['timerange'] . '</b></a></div>
+                        </div>
+                    </div>';
 
     }
     return $body;
