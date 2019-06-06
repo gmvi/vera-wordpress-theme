6c833f3308a6f6f206d92d25cefb5a3f8bc17ed7
Marianne Feng <feng.marianne@gmail.com>
2019-06-06 00:02:59 -0700

    moved ordering for artist so that on mobile, artist shows up before show info

--- single-galleries.php
+++ single-galleries.php
@@ -27,16 +27,52 @@ $container = get_theme_mod( 'understrap_container_type' );
                 //get artist information
                 $artist = get_field('artist');
 
-                //TODO: put artist at the top for mobile
                 //TODO: move title to above image on mobile
                 //TODO: limit image to be banner, line up columns side by side
-
 
                 //TODO: keep tablet half half (Jessi will work on decreasing size for tablet + mobile), make sure this looks good on tablet
 
 				?>
                 <div id="content" class="entry-content container">
                     <div class="row p-4 py-sm-4 px-sm-0 ml-1 no-gutters">
+                        <div class="col-sm-3 order-md-12">
+                            <div class="card mx-auto artist mb-4">
+			                    <?php
+
+			                    if (trim($artist['photo']) !== '') {
+				                    echo "<img class=\"card-img-top\" src=\"" . $artist['photo'] . "\" alt=\"" . $artist['title'] . "\">";
+			                    }
+
+			                    ?>
+
+                                <div class="card-footer">
+                                    <h1 class="card-title mb-2"><?php echo $artist['title']; ?></h1>
+                                    <p class="card-text mt-2"><?php echo $artist['bio']; ?></p>
+                                    <div class="artist-social vera-bordered-social-icons">
+					                    <?php if (!empty($artist['social'])) {
+						                    foreach($artist['social'] as $social) {
+							                    echo "<a href=\"" . $social['link'] . "\" target=\"_blank\">";
+
+							                    switch ($social['type']) {
+								                    case 'Facebook':
+									                    echo "<i class=\"fa fa-facebook-f text-primary\"></i>";
+									                    break;
+								                    case 'Instagram':
+									                    echo "<i class=\"fa fa-instagram text-primary\"></i>";
+									                    break;
+								                    case 'Twitter':
+									                    echo "<i class=\"fa fa-twitter text-primary\"></i>";
+									                    break;
+							                    }
+
+							                    echo "</a>";
+						                    }
+					                    } ?>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                         <div class="col-sm-9 pb-3 blog-contents gallery-contents">
 							<?php the_content()?>
                             <a class="btn bordered-button btn-outline-primary" href="<?php the_field('gallery_button_link'); ?>"><?php the_field('gallery_button_label')?></a>
@@ -56,44 +92,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                             ?>
 
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="card m-auto" style="width: 18rem;">
-                                <?php
-
-                                if (trim($artist['photo']) !== '') {
-                                    echo "<img class=\"card-img-top\" src=\"" . $artist['photo'] . "\" alt=\"" . $artist['title'] . "\">";
-                                }
-
-                                ?>
-
-                                <div class="card-footer">
-                                    <h1 class="card-title mb-2"><?php echo $artist['title']; ?></h1>
-                                    <p class="card-text mt-2"><?php echo $artist['bio']; ?></p>
-                                    <div class="artist-social vera-bordered-social-icons">
-                                        <?php if (!empty($artist['social'])) {
-                                            foreach($artist['social'] as $social) {
-                                                echo "<a href=\"" . $social['link'] . "\" target=\"_blank\">";
-
-	                                            switch ($social['type']) {
-		                                            case 'Facebook':
-                                                        echo "<i class=\"fa fa-facebook-f text-primary\"></i>";
-			                                            break;
-		                                            case 'Instagram':
-			                                            echo "<i class=\"fa fa-instagram text-primary\"></i>";
-			                                            break;
-		                                            case 'Twitter':
-			                                            echo "<i class=\"fa fa-twitter text-primary\"></i>";
-			                                            break;
-	                                            }
-
-	                                            echo "</a>";
-                                            }
-                                        } ?>
-                                    </div>
-
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
